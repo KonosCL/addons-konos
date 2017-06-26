3ae7732c0ec3c2faceee3f93505527047c916ec9
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2017-06-26 12:59:54 -0400

    Add Chart of Account

---
+++ l10n_cl_chart_of_account/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2017 Konos Soluciones y Servicios Limitada http://www.konos.cl
+
+{
+    'name': 'Chile Localization Chart Account SII',
+    'version': '1.0',
+    'description': """
+Chilean accounting chart and tax localization.
+==============================================
+Plan contable chileno e impuestos de acuerdo a disposiciones vigentes,
+basado en plan de cuentas del Servicio de Impuestos Internos
+
+    """,
+    'author': 'Konos',
+    'website': 'http://www.konos.cl',
+    'category': 'Localization',
+    'depends': ['account'],
+    'data': [
+        'data/l10n_cl_chart_of_account_data.xml',
+        'data/account_tax_data.xml',
+        'data/account_chart_template_data.yml',
+    ],
+}
+
