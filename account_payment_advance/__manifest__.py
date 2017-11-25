612b38d03a4d40de9c63220db3551accb3b66fcd
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2017-11-24 22:58:35 -0300

    Add V10 Account Advance Payment

---
+++ account_payment_advance/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Konos <info@konos.cl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Payment Advance", 
+    "version": "10.1.1", 
+    "author": "Konos", 
+    "category": "Generic Modules/Accounting", 
+    "description": """
+Account Payment Advance
+=======================
+
+This module allows the user to select an account before entering a payment.
+
+    """, 
+    "website": "http://www.konos.cl", 
+    "license": "AGPL-3", 
+    "depends": [
+        "account"
+    ], 
+    "demo": [], 
+    "data": [
+        
+        "view/account_payment_advance.xml"
+    ], 
+    "test": [], 
+    "js": [], 
+    "css": [], 
+    "qweb": [], 
+    "installable": True, 
+    "auto_install": False, 
+    "active": False
+}
---
+++ account_payment_advance/model/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Konos <info@konos.cl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import account_payment
+
+
+
