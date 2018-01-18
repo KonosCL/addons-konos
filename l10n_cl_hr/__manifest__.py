e5d6d509699cc5de065bde1197255319e9a7b066
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2018-01-18 13:55:32 -0300

    [UPD] Security Access Model

--- l10n_cl_hr/__manifest__.py
+++ l10n_cl_hr/__manifest__.py
@@ -1,23 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Chilean Payroll & Human Resources',
     'category': 'Localization',
@@ -34,8 +18,8 @@
     'license': 'AGPL-3',
     'version': '10.0.9',
     'description': """
-Chilean Payroll Salary Rules.
-============================
+Chilean Payroll & Human Resources.
+==================================
 
     -Payroll configuration for Chile localization.
     -All contributions rules for Chile payslip.
@@ -50,11 +34,12 @@ Chilean Payroll Salary Rules.
     * Previred Plain Text
     , ...
     Report
-    """,
+  """,
     'active': True,
     'data': [
         'views/hr_indicadores_previsionales_view.xml',
         'views/hr_salary_rule_view.xml',
+
         'views/hr_contract_view.xml',
         'views/hr_employee.xml',
         'views/hr_payslip_view.xml',
@@ -66,6 +51,7 @@ Chilean Payroll Salary Rules.
         'views/hr_salary_books.xml',
         'data/l10n_cl_hr_payroll_data.xml',
         'wizard/wizard_export_csv_previred_view.xml',
+        'security/ir.model.access.csv',
     ],
     'demo': ['demo/l10n_cl_hr_payroll_demo.xml'],
     'installable': True,
