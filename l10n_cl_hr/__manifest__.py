27ea7a97b126d224c4f2f03707ffaed728baebe4
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-03-14 01:21:41 -0300

    [FIX] Mostrar Horas  extras y horas descuento en reporte

--- l10n_cl_hr/__manifest__.py
+++ l10n_cl_hr/__manifest__.py
@@ -25,7 +25,7 @@
         "Francisco Lorca <fcolorca1979@gmail.com>",
     ],
     'license': 'AGPL-3',
-    'version': '11.0.1.0.5',
+    'version': '11.0.1.0.6',
     'description': """
 Chilean Payroll & Human Resources.
 ==================================
