5e564b38053d13aa554eb55689bdc5ed8db2770a
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-03-14 22:36:38 -0300

    [MIG]Balance 8 Columnas

---
+++ l10n_cl_balance/__manifest__.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2018 Konos Soluciones y Servicios Limitada http://www.konos.cl
+{
+    'name':'Chilean 8 Column Balance',
+    'description':'Formato de Balance de 8 Columnas para Chile',
+    'category': 'Localization/Chile',
+    'version':'0.1',
+    'author':'Konos',
+    'website':'http://konos.cl',
+    'data': [
+            'data/report_paperformat.xml',
+            'views/layout.xml',
+    ],
+    'depends': [
+                'account',
+            ],
+}
