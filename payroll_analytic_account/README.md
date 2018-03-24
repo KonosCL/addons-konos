eead407a1cb89f44a08b9c4b195238a9318accd8
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2018-03-24 13:36:04 -0300

    [ADD] Initial Commit

---
+++ payroll_analytic_account/README.md
@@ -0,0 +1,19 @@
+Odoo - Payroll Anaylitic Account
+========================================
+
+When enable in an specific Salary Rule it uses the Analytic Account selected in the Employees Contract
+You can check functionality in the following video:
+
+https://www.youtube.com/watch?v=raLA72Sfcds
+
+## Credits
+<p>
+<img width="200" alt="Logo Konos" src="http://www.konos.cl/web/image/666" />
+</p>
+**Konos** - http://konos.cl
+ - Nelson Ramírez <info@konos.cl>
+
+
+ 
+ **Contributors**
+ - Daniel Blanco Martín <daniel@blancomartin.com>
---
+++ payroll_analytic_account/models/hr_payroll_analytic_account.py
@@ -0,0 +1,144 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    odoo, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    d$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_is_zero
+from odoo.tools.translate import _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+
+class HrPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+
+    @api.multi
+    def action_payslip_done(self):
+        precision = self.env['decimal.precision'].precision_get('Payroll')
+
+        for slip in self:
+            line_ids = []
+            debit_sum = 0.0
+            credit_sum = 0.0
+            date = slip.date or slip.date_to
+
+            name = _('Payslip of %s') % (slip.employee_id.name)
+            move_dict = {
+                'narration': name,
+                'ref': slip.number,
+                'journal_id': slip.journal_id.id,
+                'date': date,
+            }
+            for line in slip.details_by_salary_rule_category:
+                cost_center = False
+                amount = slip.credit_note and -line.total or line.total
+                if float_is_zero(amount, precision_digits=precision):
+                    continue
+                debit_account_id = line.salary_rule_id.account_debit.id
+                credit_account_id = line.salary_rule_id.account_credit.id
+                
+                if line.salary_rule_id.account_analytic_true:
+                    cost_center = slip.contract_id.analytic_account_id and slip.contract_id.analytic_account_id.id or False
+                else:
+                    cost_center = line.salary_rule_id.analytic_account_id and line.salary_rule_id.analytic_account_id.id or False
+
+
+                if debit_account_id:
+                    debit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=False),
+                        'account_id': debit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount > 0.0 and amount or 0.0,
+                        'credit': amount < 0.0 and -amount or 0.0,
+                        'analytic_account_id': cost_center,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(debit_line)
+                    debit_sum += debit_line[2]['debit'] - debit_line[2]['credit']
+
+                if credit_account_id:
+                    credit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=True),
+                        'account_id': credit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount < 0.0 and -amount or 0.0,
+                        'credit': amount > 0.0 and amount or 0.0,
+                        'analytic_account_id': cost_center,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(credit_line)
+                    credit_sum += credit_line[2]['credit'] - credit_line[2]['debit']
+
+            if float_compare(credit_sum, debit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_credit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Credit Account!') % (slip.journal_id.name))
+                adjust_credit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': 0.0,
+                    'credit': debit_sum - credit_sum,
+                })
+                line_ids.append(adjust_credit)
+
+            elif float_compare(debit_sum, credit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_debit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Debit Account!') % (slip.journal_id.name))
+                adjust_debit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': credit_sum - debit_sum,
+                    'credit': 0.0,
+                })
+                line_ids.append(adjust_debit)
+            move_dict['line_ids'] = line_ids
+            move = self.env['account.move'].create(move_dict)
+            slip.write({'move_id': move.id, 'date': date})
+            move.post()
+        return super(HrPayslip, self).action_payslip_done()
+
+
+
+
+
+class HrSalaryRule(models.Model):
+    _inherit = 'hr.salary.rule'
+    account_analytic_true = fields.Boolean('Analytic Account in Contract')
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
