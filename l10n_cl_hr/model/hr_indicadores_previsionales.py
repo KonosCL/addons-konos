577bd136d8eea17388c39c5f5f4e530bbc1f34ee
Nelson Ramírez Sánchez <info@konos.cl>
2018-12-27 09:09:09 -0300

    [UPD] Previred 100% Functional

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -31,6 +31,8 @@
 ##############################################################################
 
 from odoo import api, fields, models, tools, _
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from datetime import datetime
 import logging
 import requests
@@ -209,55 +211,99 @@ class hr_indicadores_previsionales(models.Model):
 
 
 
-    def clean_string(self, s):
-        s = s.replace("<strong>", "")
-        s = s.replace("</strong>", "")
-        s = s.replace("</td>", "")
-        s = s.replace("<td>", "")
-        s = s.replace("$ ", "")        
-        s = s.replace("</div>", "")
-        s = s.replace("<div>", "")
-        s = s.replace(".", "")
-        s = s.replace(" ", "")
-        s = s.replace("%", "")
-        s = s.replace("RI", "")
-        s = s.replace("</tr>", "")
-        s = s.replace("<tr>", "")
-        s = s.replace('<tdwidth="20">', '')
-        s = s.replace("<tbody>", "")
-        s = s.replace("</tbody>", "")
-        s = s.replace("</table>", "")
-        s = s.replace("<table>", "")
-        s = s.replace('<tdwidth="80">', '')
-        s = s.replace('colspan="2">', '')
-        s = s.replace("width", "")
-        s = s.replace('"', '')
-
-        
-        return s
-
-
     @api.one
     def update_document(self):
-        url = "https://www.previred.com/web/previred/indicadores-previsionales"
-        response = requests.get(url,params="nombre")
-        if response and response.status_code!=200:
-            _logger.warning("error %s" %(response))
-            vals= {'detail':"Not found."}
-        else:
-            vals = response.text
-            vals = self.clean_string(vals)
-            fecha = self.find_month(self.month) + str(self.year)
-            fecha = "de" + fecha.strip()
-            try:
-                _logger.warning(vals)
-                self.uf = self.find_between_r( vals, fecha, '<' ).replace(",", ".")[:8]
-                self.utm = self.find_between_r( vals, '2018<tdalign=center>', '<' )[:5]
-                self.uta = self.find_between_r( vals, '2018<tdalign=center>', '<divclass=entry-links>' )[21:27]
-                #self.tope_imponible_afp = self.find_between_r( vals, 'ParaafiliadosaunaAFP(', 'UF):' )[:5]
+        self.update_date = datetime.today()
+        try:
+            html_doc = urlopen('https://www.previred.com/web/previred/indicadores-previsionales').read()
+            soup = BeautifulSoup(html_doc, 'html.parser')
 
-            except ValueError:
-                return ""
+            letters = soup.find_all("table")
+
+            def clear_string(cad):
+                cad = cad.replace(".", '').replace("$", '').replace(" ", '')
+                cad = cad.replace("Renta", '').replace("<", '').replace(">", '')
+                cad = cad.replace("=", '').replace("R", '').replace("I", '').replace("%", '')
+                cad = cad.replace(",", '.')
+                cad = cad.replace("1ff8","")
+                return cad
+        except ValueError:
+            return ""
+
+        def string_divide(cad, cad2, rounded):
+            return round(float(cad) / float(cad2), rounded)
+
+
+        try:
+            # UF
+            self.uf = clear_string(letters[0].select("strong")[1].get_text())
+
+            # 1 UTM
+            self.utm = clear_string(letters[1].select("strong")[3].get_text())
+
+            # 1 UTA
+            self.uta = clear_string(letters[1].select("strong")[4].get_text())
+
+            # 3 RENTAS TOPES IMPONIBLES UF
+            self.tope_imponible_afp = string_divide(clear_string(letters[2].select("strong")[1].get_text()), self.uf, 2)
+            self.tope_imponible_ips = string_divide(clear_string(letters[2].select("strong")[2].get_text()), self.uf, 2)
+            self.tope_imponible_seguro_cesantia = string_divide(clear_string(letters[2].select("strong")[3].get_text()),
+                                                                self.uf, 2)
+
+            # 4 RENTAS TOPES IMPONIBLES
+            self.sueldo_minimo = clear_string(letters[3].select("strong")[1].get_text())
+            self.sueldo_minimo_otro = clear_string(letters[3].select("strong")[2].get_text())
+
+            # Ahorro Previsional Voluntario
+            self.tope_mensual_apv = string_divide(clear_string(letters[4].select("strong")[2].get_text()), self.uf, 2)
+            self.tope_anual_apv = string_divide(clear_string(letters[4].select("strong")[1].get_text()), self.uf, 2)
+
+            # 5 DEPÓSITO CONVENIDO
+            self.deposito_convenido = string_divide(clear_string(letters[5].select("strong")[1].get_text()), self.uf, 2)
+
+            # 6 RENTAS TOPES IMPONIBLES
+            self.contrato_plazo_indefinido_empleador = clear_string(letters[6].select("strong")[5].get_text())
+            self.contrato_plazo_indefinido_trabajador = clear_string(letters[6].select("strong")[6].get_text())
+            self.contrato_plazo_fijo_empleador = clear_string(letters[6].select("strong")[7].get_text())
+            self.contrato_plazo_indefinido_empleador_otro = clear_string(letters[6].select("strong")[9].get_text())
+
+            # 7 ASIGNACIÓN FAMILIAR
+            self.asignacion_familiar_monto_a = clear_string(letters[8].select("strong")[4].get_text())
+            self.asignacion_familiar_monto_b = clear_string(letters[8].select("strong")[6].get_text())
+            self.asignacion_familiar_monto_c = clear_string(letters[8].select("strong")[8].get_text())
+
+            self.asignacion_familiar_primer = clear_string(letters[8].select("strong")[5].get_text())[1:]
+            self.asignacion_familiar_segundo = clear_string(letters[8].select("strong")[7].get_text())[6:]
+            self.asignacion_familiar_tercer = clear_string(letters[8].select("strong")[9].get_text())[6:]
+
+            # 8 TASA COTIZACIÓN OBLIGATORIO AFP
+            self.tasa_afp_capital = clear_string(letters[7].select("strong")[8].get_text())
+            self.tasa_sis_capital = clear_string(letters[7].select("strong")[9].get_text())
+
+            self.tasa_afp_cuprum = clear_string(letters[7].select("strong")[11].get_text().replace(" ", '').replace("%", '').replace("1ff8", ''))
+            self.tasa_sis_cuprum = clear_string(letters[7].select("strong")[12].get_text())
+
+            self.tasa_afp_habitat = clear_string(letters[7].select("strong")[14].get_text())
+            self.tasa_sis_habitat = clear_string(letters[7].select("strong")[15].get_text())
+
+            self.tasa_afp_planvital = clear_string(letters[7].select("strong")[17].get_text())
+            self.tasa_sis_planvital = clear_string(letters[7].select("strong")[18].get_text())
+
+            self.tasa_afp_provida = clear_string(letters[7].select("strong")[20].get_text().replace(" ", '').replace("%", '').replace("1ff8", ''))
+            self.tasa_sis_provida = clear_string(letters[7].select("strong")[21].get_text())
+
+            self.tasa_afp_modelo = clear_string(letters[7].select("strong")[23].get_text())
+            self.tasa_sis_modelo = clear_string(letters[7].select("strong")[24].get_text())
+
+            self.tasa_independiente_capital = clear_string(letters[7].select("strong")[10].get_text())[:5]
+            self.tasa_independiente_cuprum = clear_string(letters[7].select("strong")[13].get_text())
+            self.tasa_independiente_habitat = clear_string(letters[7].select("strong")[16].get_text())
+            self.tasa_independiente_planvital = clear_string(letters[7].select("strong")[19].get_text())
+            self.tasa_independiente_provida = clear_string(letters[7].select("strong")[22].get_text())
+            self.tasa_independiente_modelo = clear_string(letters[7].select("strong")[25].get_text())
+
+        except ValueError:
+            return ""
 
 
             
