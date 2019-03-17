b4b99b37a3b15f0fd1f13226738becbb7842be48
Nelson Ramírez Sánchez <info@konos.cl>
2019-03-17 20:08:29 -0300

    [ADD] Isapre Bco Estado

--- l10n_cl_hr/__manifest__.py
+++ l10n_cl_hr/__manifest__.py
@@ -25,7 +25,7 @@
         "Francisco Lorca <fcolorca1979@gmail.com>",
     ],
     'license': 'AGPL-3',
-    'version': '11.0.1.4.0',
+    'version': '11.0.1.4.1',
     'description': """
 Chilean Payroll & Human Resources.
 ==================================
