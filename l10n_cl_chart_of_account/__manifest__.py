7c6fc75d77400ae3e94d52ea422fc07bee75f68d
Daniel Santibáñez Polanco <dansanti@gmail.com>
2020-01-24 15:41:04 -0300

    [IMP]Cálculo Impuesto fijo en base a unidad de medida

--- l10n_cl_chart_of_account/__manifest__.py
+++ l10n_cl_chart_of_account/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Chile Localization Chart Account SII',
-    'version': '1.0',
+    'version': '1.10.0',
     'description': """
 Chilean accounting chart and tax localization.
 ==============================================
@@ -16,11 +16,12 @@ basado en plan de cuentas del Servicio de Impuestos Internos
     'author': 'Konos',
     'website': 'http://www.konos.cl',
     'category': 'Localization',
-    'sequence': 50,    
+    'sequence': 50,
     'license': 'AGPL-3',
     'depends': ['account'],
     'data': [
         'views/account_tax.xml',
+        'data/product_uom.xml',
         'data/l10n_cl_chart_of_account_data.xml',
         'data/account_tax_data.xml',
         'data/account_chart_template_data.xml',
--- l10n_cl_chart_of_account/models/account_invoice.py
+++ l10n_cl_chart_of_account/models/account_invoice.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import api, models, fields, _
 
 
@@ -5,14 +6,14 @@ class AccountInvoiceTax(models.Model):
     _inherit = "account.invoice.tax"
 
     amount_retencion = fields.Monetary(
-        string="Retención",
-        default=0.00,
-    )
+            string="Retención",
+            default=0.00,
+        )
     retencion_account_id = fields.Many2one(
-        'account.account',
-        string='Retention Account',
-        domain=[('deprecated', '=', False)],
-    )
+            'account.account',
+            string='Tax Account',
+            domain=[('deprecated', '=', False)],
+        )
 
     def _getNeto(self, currency):
         neto = 0
@@ -20,54 +21,101 @@ class AccountInvoiceTax(models.Model):
             base = tax.base
             price_tax_included = 0
             #amount_tax +=tax.amount
-            for line in tax.invoice_id.invoice_line_ids:
-                if tax.tax_id in line.invoice_line_tax_ids and tax.tax_id.price_include:
-                    price_tax_included += line.price_total
-            if price_tax_included > 0 and  tax.tax_id.sii_type in ["R"] and tax.tax_id.amount > 0:
-                base = currency.round(price_tax_included)
-            elif price_tax_included > 0 and tax.tax_id.amount > 0:
-                base = currency.round(price_tax_included / ( 1 + tax.tax_id.amount / 100.0))
+            if tax.tax_id.amount_type == 'percent':
+                for line in tax.invoice_id.invoice_line_ids:
+                    if tax.tax_id in line.invoice_line_tax_ids and tax.tax_id.price_include:
+                        price_tax_included += line.price_total
+                if price_tax_included > 0 and tax.tax_id.sii_type in ["R"] and tax.tax_id.amount > 0:
+                    base = currency.round(price_tax_included)
+                elif price_tax_included > 0 and tax.tax_id.amount > 0:
+                    base = currency.round(price_tax_included / ( 1 + tax.tax_id.amount / 100.0))
             neto += base
         return neto
 
 
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    @api.one
+    @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
+        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
+        'invoice_id.date_invoice', 'invoice_id.date')
+    def _compute_price(self):
+        currency = self.invoice_id and self.invoice_id.currency_id or None
+        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
+        taxes = False
+        if self.invoice_line_tax_ids:
+            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id, uom_id=self.uom_id)
+        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
+        self.price_total = taxes['total_included'] if taxes else self.price_subtotal
+        if self.invoice_id.currency_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
+            currency = self.invoice_id.currency_id
+            date = self.invoice_id._get_currency_rate_date()
+            price_subtotal_signed = currency._convert(price_subtotal_signed, self.invoice_id.company_id.currency_id, self.company_id or self.env.user.company_id, date or fields.Date.today())
+        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.price_subtotal_signed = price_subtotal_signed * sign
+
+
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-
-    def _compute_amount(self):
-        #@TODO Buscar una mejor forma de aplicar retención
-        for inv in self:
-            amount_retencion = 0
-            neto = 0
-            amount_tax = 0
-            included = False
-            for tax in inv.tax_line_ids:
-                if tax.tax_id.price_include:
-                    included = True
-                amount_tax += tax.amount
-                amount_retencion  += tax.amount_retencion
-            inv.amount_retencion = amount_retencion
-            if included:
-                neto += inv.tax_line_ids._getNeto(inv.currency_id)
-                amount_retencion += amount_retencion
-            else:
-                neto += sum(line.price_subtotal for line in inv.invoice_line_ids)
-            inv.amount_untaxed = neto
-            inv.amount_tax = amount_tax
-            inv.amount_total = inv.amount_untaxed + inv.amount_tax - amount_retencion
-            amount_total_company_signed = inv.amount_total
-            amount_untaxed_signed = inv.amount_untaxed
-            if inv.currency_id and inv.currency_id != inv.company_id.currency_id:
-                currency_id = inv.currency_id.with_context(date=inv.date_invoice)
-                amount_total_company_signed = currency_id.compute(inv.amount_total, inv.company_id.currency_id)
-                amount_untaxed_signed = inv.currency_id.compute(inv.amount_untaxed, inv.company_id.currency_id)
-            sign = inv.type in ['in_refund', 'out_refund'] and -1 or 1
-            inv.amount_total_company_signed = amount_total_company_signed * sign
-            inv.amount_total_signed = inv.amount_total * sign
-            inv.amount_untaxed_signed = amount_untaxed_signed * sign
 
     amount_retencion = fields.Monetary(
         string="Retención",
         default=0.00,
         compute='_compute_amount',
     )
+
+    @api.one
+    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding',
+                 'currency_id', 'company_id', 'date_invoice', 'type')
+    def _compute_amount(self):
+        #@TODO Buscar una mejor forma de aplicar retención
+        amount_retencion = 0
+        neto = 0
+        amount_tax = 0
+        included = False
+        for tax in self.tax_line_ids:
+            if tax.tax_id.price_include:
+                included = True
+            amount_tax += tax.amount
+            amount_retencion += tax.amount_retencion
+        self.amount_retencion = amount_retencion
+        if included:
+            neto += self.tax_line_ids._getNeto(self.currency_id)
+            amount_retencion += amount_retencion
+        else:
+            neto += sum(line.price_subtotal for line in self.invoice_line_ids)
+        self.amount_untaxed = neto
+        self.amount_tax = amount_tax
+        self.amount_total = neto + amount_tax - amount_retencion
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
+            currency_id = self.currency_id
+            amount_total_company_signed = currency_id._convert(self.amount_total, self.company_id.currency_id, self.company_id, self.date_invoice or fields.Date.today())
+            amount_untaxed_signed = currency_id._convert(self.amount_untaxed, self.company_id.currency_id, self.company_id, self.date_invoice or fields.Date.today())
+        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.amount_total_company_signed = amount_total_company_signed * sign
+        self.amount_total_signed = self.amount_total * sign
+        self.amount_untaxed_signed = amount_untaxed_signed * sign
+
+
+    @api.multi
+    def get_taxes_values(self):
+        tax_grouped = {}
+        round_curr = self.currency_id.round
+        for line in self.invoice_line_ids:
+            price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id, uom_id=line.uom_id)['taxes']
+            for tax in taxes:
+                val = self._prepare_tax_line_vals(line, tax)
+                key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
+
+                if key not in tax_grouped:
+                    tax_grouped[key] = val
+                    tax_grouped[key]['base'] = round_curr(val['base'])
+                else:
+                    tax_grouped[key]['amount'] += val['amount']
+                    tax_grouped[key]['base'] += round_curr(val['base'])
+        return tax_grouped
