1fc20ef9f7079aea4b05a0c41ae591ad6ae48964
Daniel Santibáñez Polanco <dansanti@gmail.com>
2021-01-27 21:33:50 -0300

    Auto install según odoo 14.0

--- l10n_cl_chart_of_account/__init__.py
+++ l10n_cl_chart_of_account/__init__.py
@@ -3,19 +3,31 @@
 
 # Copyright (c) 2017 Konos http://www.konos.cl
 
+from . import controllers
 from . import models
+from . import wizard
+from . import report
+
 from odoo import api, SUPERUSER_ID
+from odoo.addons.account import SYSCOHADA_LIST
 
-SYSCOHADA_LIST = ['BJ', 'BF', 'CM', 'CF', 'KM', 'CG', 'CI', 'GA', 'GN', 'GW', 'GQ', 'ML', 'NE', 'CD', 'SN', 'TD', 'TG']
 
-def _auto_install_l10n(cr, registry):
+
+def _auto_install_l10n(env):
     #check the country of the main company (only) and eventually load some module needed in that country
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    country_code = env.user.company_id.country_id.code
+    country_code = env.company.country_id.code
     if country_code:
         #auto install localization module(s) if available
+        to_install_l10n = env['ir.module.module'].search_count([
+            ('category_id', '=', env.ref('base.module_category_accounting_localizations_account_charts').id),
+            ('state', '=', 'to install'),
+        ])
         module_list = []
-        if country_code in SYSCOHADA_LIST:
+        if to_install_l10n:
+            # We don't install a CoA if one was passed in the command line
+            # or has been selected to install
+            pass
+        elif country_code in SYSCOHADA_LIST:
             #countries using OHADA Chart of Accounts
             module_list.append('l10n_syscohada')
         elif country_code == 'GB':
@@ -23,35 +35,26 @@ def _auto_install_l10n(cr, registry):
         elif country_code == 'DE':
             module_list.append('l10n_de_skr03')
             module_list.append('l10n_de_skr04')
-        elif country_code == 'CN':
-            module_list.append('l10n_cn_small_business')
-            module_list.append('l10n_cn_standard')
+        elif country_code == 'CL':
+            module_list.append('l10n_cl_chart_of_account')
         else:
-            if country_code.lower() == 'cl' and env['ir.module.module'].search([('name', '=', 'l10n_cl_chart_of_account')]):
-                module_list.append('l10n_cl_chart_of_account')
-            elif env['ir.module.module'].search([('name', '=', 'l10n_' + country_code.lower())]):
+            if env['ir.module.module'].search([('name', '=', 'l10n_' + country_code.lower())]):
                 module_list.append('l10n_' + country_code.lower())
             else:
                 module_list.append('l10n_generic_coa')
         if country_code == 'US':
             module_list.append('account_plaid')
-            module_list.append('l10n_us_check_printing')
-        if country_code == 'CA':
-            module_list.append('l10n_ca_check_printing')
-        if country_code in ['US', 'AU', 'NZ', 'CA', 'CO', 'EC', 'ES', 'FR', 'IN', 'MX', 'UK']:
+        if country_code in ['US', 'CA']:
+            module_list.append('account_check_printing')
+        if country_code in ['US', 'AU', 'NZ', 'CA', 'CO', 'EC', 'ES', 'FR', 'IN', 'MX', 'GB']:
             module_list.append('account_yodlee')
         if country_code in SYSCOHADA_LIST + [
             'AT', 'BE', 'CA', 'CO', 'DE', 'EC', 'ES', 'ET', 'FR', 'GR', 'IT', 'LU', 'MX', 'NL', 'NO',
-            'PL', 'PT', 'RO', 'SI', 'TR', 'UK', 'VE', 'VN'
+            'PL', 'PT', 'RO', 'SI', 'TR', 'GB', 'VE', 'VN'
             ]:
             module_list.append('base_vat')
+        if country_code == 'MX':
+            module_list.append('l10n_mx_edi')
 
-        # European countries will be using SEPA
-        europe = env.ref('base.europe', raise_if_not_found=False)
-        if europe:
-            europe_country_codes = [x.code for x in europe.country_ids]
-            if country_code in europe_country_codes:
-                module_list.append('account_sepa')
-                module_list.append('account_bank_statement_import_camt')
         module_ids = env['ir.module.module'].search([('name', 'in', module_list), ('state', '=', 'uninstalled')])
         module_ids.sudo().button_install()
